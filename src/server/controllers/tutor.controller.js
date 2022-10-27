033c95dcd74d77a966c12dda6a17387611b0505a
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2022-10-27 18:45:46 -0400

    tutor pefil object created

--- src/server/controllers/tutor.controller.js
+++ src/server/controllers/tutor.controller.js
@@ -1,4 +1,6 @@
 const Tutor = require("../database/sequalize/models/students/studentTutor.model.js");
+const StudentList = require("../database/sequalize/models/students/studentList.model");
+const Grades = require("../database/sequalize/models/students/grades.model")
 const sequelize = require("../database/sequalize/connection.js");
 
 async function registerTutor(req, res){
@@ -57,5 +59,64 @@ async function getTutor(req, res){
     }
 }
 
+async function getTutorPerfil(req, res){
+    let id = req.session.tutorID;
+    const perfilTransaction = await sequelize.transaction();
+    try {
+        let tutor = await Tutor.findAll({
+            where:{
+                id
+            },
+            transaction: perfilTransaction
+        });
 
-module.exports ={registerTutor, getTutor}
+        if(tutor.length <= 0){
+            res.status(404).json({error: "El tutor no está registrado"});
+            return
+        }
+
+        let studens = await StudentList.findAll({
+            where:{
+                tutorId: id
+            },
+            transaction: perfilTransaction
+        });
+
+        if(studens.length <= 0){
+            res.status(200).json({error: "El tutor no tiene estudiantes inscritos"});
+            return
+        }
+
+        let idList = studens.map(student=>{
+            return student.id;
+        })
+
+        let grades = await Grades.findAll({
+            where:{
+                studentId: idList
+            },
+            transaction: perfilTransaction
+        })
+  
+        await perfilTransaction.commit();
+
+        let stdGrades = studens.map(std=>{
+            let grd = grades.filter(gr => std.id === gr.studentId)
+            let obj = std;
+            obj.dataValues.subjects = grd[grd.length - 1].subjects;
+            return obj;
+        })
+
+        let data = tutor[0];
+        data.dataValues.students = stdGrades;
+        
+        res.status(200).json(data);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Ha ocurrido un error al obtener el perfil"})
+        
+    }
+}
+
+module.exports ={registerTutor, getTutor, getTutorPerfil}
--- src/server/routes/tutor.routes.js
+++ src/server/routes/tutor.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const Router = express.Router();
-const {registerTutor, getTutor} = require("../controllers/tutor.controller.js")
+const {registerTutor, getTutor, getTutorPerfil} = require("../controllers/tutor.controller.js")
 
 
 Router.post("/tutor", express.json(), registerTutor);
 
 Router.get("/tutor", express.urlencoded(), getTutor);
 
-
+Router.get("/tutor/perfil", getTutorPerfil);
 
 
 
